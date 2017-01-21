4ff4863c572001bf25380ac13266ade0d50e02bb
c00z <zcusimano@gmail.com>
2017-01-21 11:58:28 -0800

    finished 5.10

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,13 +9,16 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :text))
-
-    @article.save
-    redirect_to @article
+    @article = Article.new(article_params)
+  if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
 private
